580934522d228f58b7fdbc65952910edc6e5dab2
Aleksey Berezka <a.berezka@dodobrands.io>
2024-12-19 12:22:36 +0500

    Update to Swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.8
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -54,6 +54,5 @@ let package = Package(
                 .product(name: "FirebasePerformance", package: "firebase-ios-sdk")
             ]
         ),
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
