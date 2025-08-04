c42f156ee5dd441092310cb22da081f9f8dbd82a
Aleksey Berezka <a.berezka@dodobrands.io>
2025-08-04 09:25:09 +0500

    Bump min OS versions

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let testTargetName = targetName + "Tests"
 let package = Package(
     name: packageName,
     platforms: [
-        .iOS(.v12),
-        .tvOS(.v13)
+        .iOS(.v15),
+        .tvOS(.v15)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
